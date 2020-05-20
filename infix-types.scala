62e4dba753657f3fa5ecff9177d409a3dd23bea6
Phani <itsmephani@users.noreply.github.com>
2020-05-20 22:04:21 +0530

    Update infix-types.scala

--- infix-types.scala
+++ infix-types.scala
@@ -3,7 +3,7 @@ case class Game(name: String)
 class Plays[A, B](val a: A, val b: B)
 
 def kidPlays(kidPlaying: Kid Plays Game) =
-  //Notice our type: Person loves Person!
+  //Notice our type: Kid plays Game!
   kidPlaying.a.name + " is playing " + kidPlaying.b.name
 
 val kid = new Kid("Rohit")
