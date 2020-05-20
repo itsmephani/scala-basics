c6336602e68bee57414398e302ceddbbf7fa9bc5
Phani <itsmephani@users.noreply.github.com>
2020-05-20 17:59:56 +0530

    Create expressions.scala

---
+++ expressions.scala
@@ -0,0 +1,16 @@
+val a =  3 + 2 // Addition
+val b =  3 - 2 // Subtraction
+val c =  3 * 2 // Multiplication
+val d =  3 / 2 // Division
+
+// Operatiors
+||, &&, <, >, >>, <<
+
+val e = if (a == 5) 5 else 2 // if else expression.
+
+// Code block will return last expression in the block. code = 2
+val code = {
+  if (b == 1) 1
+  2
+}
+
