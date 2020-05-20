57c4d9e13924ff88727dfd6318d1e233236439b6
Phani <itsmephani@users.noreply.github.com>
2020-05-20 20:20:25 +0530

    Update recursion.scala

--- recursion.scala
+++ recursion.scala
@@ -15,3 +15,33 @@ def fact(n: Int) = {
   
   factHelper(x)
 }
+
+def concat(input: String, times: Int, output: String = ""): String = {
+  if (times <= 1) input + output
+  else concat(input, times - 1, input + output)
+}
+
+def isPrime(n: Int): Boolean = {
+  def isPrimeHelper(x: Int, primeTillNow: Boolean): Boolean = {
+    if (!primeTillNow) false
+    else if (x <= 1) true
+    else isPrimeHelper(x - 1, n % x != 0)
+  }
+
+  isPrimeHelper(n/2, true)
+}
+
+def fibonacci(n: Int): Int = {
+  def fibonacciHelper(x: Int, t1: Int, t2: Int): Int = {
+    if (x == n) t2
+    else fibonacciHelper(x + 1, t2, t1 + t2)
+  }
+
+  fibonacciHelper(2, 1, 1)
+}
+
+
+println(concat("Hi", 3))
+println(isPrime(3))
+println(isPrime(4))
+println(fibonacci(8))
