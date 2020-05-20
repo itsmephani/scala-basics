779b472049ea719dafb9b55d815e2c976276cf52
Phani <itsmephani@users.noreply.github.com>
2020-05-20 17:54:53 +0530

    Basics

---
+++ basics.scala
@@ -0,0 +1,19 @@
+// Variables
+val x: Int = 42 // val are immutable, can't be modified.
+
+// var is discouraged in functional type of programming as it would be lead to side effects.
+var y: Int = 1 // var can be modified.
+y = 2
+
+
+// Scala compiler can infer type
+val z = 2
+
+
+// Types
+Int
+String
+Boolean
+Float
+Double
+Short
